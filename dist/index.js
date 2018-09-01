49a8ab5969357c917522720882d85d9272dc04f5
Abdelhadi <naimi.abdelhadi.97@gmail.com>
2018-09-01 18:44:11 +0100

    webhook

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,17 @@
-console.log("hello bitch");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const PORT = process.env.PORT || 3000;
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+    console.log("hello");
+});
+app.listen(PORT, () => {
+    console.log('Node app is running on port', PORT);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("hello bitch");
+import bodyParser from 'body-parser';
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.post('/webhook', (req, res) => {  
+ 
+  const body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach((entry : any) => {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+app.listen(PORT, () => {
+  console.log('Node app is running on port', PORT);
+});
