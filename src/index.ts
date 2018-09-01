012548f9a17861b73f031bf9ba544cb03dc9ac31
Abdelhadi <naimi.abdelhadi.97@gmail.com>
2018-09-01 19:34:55 +0100

    ls

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import bodyParser from 'body-parser';
 import express, { Request, Response } from "express";
-
+import secrets from "./secrets.json";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get("/", (req,res) => {
   console.log("Got Home");
 });
+
 app.post('/webhook', (req : Request, res : Response) => {  
   console.log("Getting request from", req.ip);
   
@@ -37,7 +38,34 @@ app.post('/webhook', (req : Request, res : Response) => {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+});
 
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  const VERIFY_TOKEN = secrets.FACEBOOK_VERIFY_TOKEN;
+    
+  // Parse the query params
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
 });
 
 app.listen(PORT, () => {
