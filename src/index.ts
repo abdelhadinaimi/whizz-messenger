c12837b34f41bde877b37470a5d30a8e6e0515ef
Abdelhadi <naimi.abdelhadi.97@gmail.com>
2018-09-01 21:52:09 +0100

    refactoring & structuring

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,37 @@
 import bodyParser from 'body-parser';
-import express, { Request, Response } from "express";
-import secrets from "./secrets.json";
-const app = express();
+import dotenv from "dotenv";
+import express from "express";
+import logger from "morgan";
 
+import webhooks from './routes/webhooks';
+
+/* Pre Init */
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.load();
+}
+
+/* Initializations */
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+/* Middlewares */
+app.use(logger("dev"));
 
+
+/* Parsers */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.get("/", (req,res) => {
-  console.log("Got Home");
-});
+/* Routes */
+app.use("/webhook",webhooks);
 
-app.post('/webhook', (req : Request, res : Response) => {  
-  console.log("Getting request from", req.ip);
-  
-  const body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach((entry : any) => {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      const webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-});
-
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  const VERIFY_TOKEN = secrets.FACEBOOK_VERIFY_TOKEN;
-    
-  // Parse the query params
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
-
-app.listen(PORT, () => {
+/* Port */
+app.listen(PORT, () => {  
   console.log('Node app is running on port', PORT);
 });
+
+
+export default app;
---
+++ src/routes/webhooks.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.post("/", (req: Request, res: Response) => {
+  const body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach((entry: any) => {
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
+
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+/**
+ * Used so that Facebook can check that this is the right webhook location
+ */
+router.get("/", (req, res) => {
+  if (req.query["hub.mode"] === "subscribe" && req.query["hub.verify_token"] === process.env.WEBHOOK_TOKEN) {
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+export default router;
